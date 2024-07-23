a985e9151fdbee74bf6bea50560a2a0d25ec8b1b
cooliceycold <112804160+cooliceycold@users.noreply.github.com>
2024-07-23 16:55:39 +0800

    Update autoload.js

--- jsdelivr/gcore/random/autoload.js
+++ jsdelivr/gcore/random/autoload.js
@@ -1,8 +1,5 @@
-// 注意：live2d_path 参数应使用绝对路径
-// const live2d_path =
-//   "https://fastly.jsdelivr.net/gh/nova1751/live2d-widget@latest/";
 
-const live2d_path = "https://jsd.cdn.zzko.cn/gh/nova1751/live2d-api@latest/";
+const live2d_path = "https://gcore.jsdelivr.net/gh/cooliceycold/live2d/";
 
 // 封装异步加载资源的方法
 function loadExternalResource(url, type) {
@@ -28,13 +25,13 @@ function loadExternalResource(url, type) {
 if (screen.width >= 768) {
   Promise.all([
     loadExternalResource(live2d_path + "live2d.min.js", "js"),
-    loadExternalResource(live2d_path + "jsdelivr/random/waifu-tips.js", "js"),
+    loadExternalResource(live2d_path + "jsdelivr/gcore/random/waifu-tips.js", "js"),
   ]).then(() => {
     // 配置选项的具体用法见 README.md
     initWidget({
       waifuPath: live2d_path + "waifu-tips.json",
 
-      // apiPath: "https://live2d.fghrsh.net/api/",
+
       cdnPath: live2d_path,
 
       tools: [
